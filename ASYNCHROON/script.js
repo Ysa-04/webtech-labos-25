be1c935ccc69cb2430ee4e60e4157ba8aed4e209
Ysa-04 <ysaura2903@outlook.com>
2025-12-02 10:53:00 +0100

    eerste deel les

--- ASYNCHROON/script.js
+++ ASYNCHROON/script.js
@@ -1,3 +1,4 @@
+/* SET TIMEOUT */
 /*
 taken die je wil doen op je vrije dag
 - stofzuigen
@@ -23,3 +24,83 @@ setTimeout(() => {
     console.log("afwasmachine is klaar!");
     }, 2000 // deze gaat VOOR de eerste setTimeout uitgevoerd worden omdat het programma hier kijkt naar de tijd die je meegeeft!!
 );
+// functie die een andere functie als parameter als input heeft --> CALLBACK FUNCTIES
+
+/* CALLBACK FUNCTIES */
+let printOutput = (thingToPrint) => {
+    console.log(thingToPrint);
+}
+let add = (a,b) => {
+    printOutput(a + b);
+}
+add(5,5);
+// de add functie heeft een dependency op printOutput -> als die naar een andere file verplaatst wordt is die out of reach bv (geen pure function dus)
+// oplossing (functie puur maken):
+let add2 = (a,b, callbackFn) => {
+    let sum = a + b;
+    callbackFn(sum); //weet niet wat callbackFn doet, geeft dit gewoon door
+}
+add2(5,5);
+
+let printOutputToAlert = (thingToPrint) => {
+    alert(thingToPrint);
+}
+add2(5,5, printOutputToAlert);
+
+//inline:
+add3(5,5, (result) => 
+    console.log(result)
+);
+document.addEventListener("click", (event) => {
+    event.preventDefault()
+});
+
+/* FACTORY FUNCTION */
+// functies kunnen ook een andere functie returnen
+let calculator = (operator) => {
+    if (operator === "+") {
+        return (a,b) => {return a+b}
+    }
+    if (operator === "-") {
+        return (a,b) => {return a+b}
+    }
+}
+let sum = calculator("+");
+let result = sum(5,5);
+console.log(result);
+
+/* ARRAY FUNCTIES */
+// simpele datatypes:
+let a = 5;
+let b = a;
+a = 10;
+console.log(b); //verwachte waarde van b is 5
+
+//complexe datatypes: arrays en objects
+let myArray = [1,2,3];
+let mySecondArray = myArray;
+myArray.push(4);
+console.log(mySecondArray); //verwachte waarde 123, maat het is 1234. waarom? de draad wordt niet 'doorgesneden'.ze zijn linked, om data te besparen
+//spreadoperator
+let myThirdArray = [...myArray]; // zorgt ervoor dat als je iets pusht in myArray het niet gepushed wordt in myThirdArray -> shallow copy
+
+let person = {
+    name: "philippe",
+    occupation: "teacher",
+    address: {
+        street: "yeetstreet",
+        number: 123,
+        city: "hornytown"
+    }
+}
+
+let secondPerson = person;
+person.name = "andie";
+person.address.street = "yoinkietoinkie st"
+console.log(secondPerson); //eerste persoon aangepast maar de tweede persoon verandert mee
+
+let thirdPerson = {...person}; //shallow copy
+console.log(thirdPerson); //blijft Philippe
+
+console.log(thirdPerson.address.street); //verandert mee, is shallow copy, geen DEEPcopy
+thirdPerson = {...person, adress: {...person.address}} //adres verandert niet mee. geneste shallow copy
